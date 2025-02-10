7f2029b06f937b9c3a968cb497aaf10ef22dc4f6
akshatg110 <54480357+akshatg110@users.noreply.github.com>
2025-02-10 09:31:14 +0530

    Update CalculatorTest.java

--- src/test/java/com/github/stokito/unitTestExample/calculator/CalculatorTest.java
+++ src/test/java/com/github/stokito/unitTestExample/calculator/CalculatorTest.java
@@ -5,17 +5,17 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	@Test
-	public void testSum() {
-		// Given
-		Calculator calculator = new Calculator();
-		// When
-		int result = calculator.sum(2, 2);
-		// Then
-		if (result != 4) {   // if 2 + 2 != 4
-			Assert.fail();
-		}
-	}
+	// @Test
+	// public void testSum() {
+	// 	// Given
+	// 	Calculator calculator = new Calculator();
+	// 	// When
+	// 	int result = calculator.sum(2, 2);
+	// 	// Then
+	// 	if (result != 4) {   // if 2 + 2 != 4
+	// 		Assert.fail();
+	// 	}
+	// }
 
 	@Test
 	public void testMinus() {
